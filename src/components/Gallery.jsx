2f4dafff81d0e29866fb43d0000233d1cfe01efe
PoR <por.pkp1997@gmail.com>
2023-08-26 00:37:08 +0700

    feat: click to show large image

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,9 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ImageContext } from "../context/ImageContext.jsx"
 
 function Gallery() {
+    const [image, setImage] = useState("")
+    const [isShow, setIsShow] = useState(false)
     const context = useContext(ImageContext)
 
     const handlepage = (action) => {
@@ -13,6 +15,10 @@ function Gallery() {
         context.getImage()
     }
 
+    const handleShowImage = (src) => {
+        setImage(src)
+        setIsShow(true)
+    }
     useEffect(() => {
         context.getImage()
     })
@@ -23,7 +29,8 @@ function Gallery() {
             <div className="flex justify-center mt-2 mx-16 flex-wrap">
                 {context.images.map((item, index) => {
                     return (
-                        <div key={index} className="w-64 m-4 h-44 rounded-lg flex justify-center items-center overflow-hidden relative" >
+                        <div key={index} className="w-64 m-4 h-44 rounded-lg flex justify-center items-center overflow-hidden relative"
+                            onClick={() => handleShowImage(item.src.large)}>
                             <img src={item.src.medium} className="min-w-full min-h-full rounded-lg hover:scale-110 duration-500" />
                             <div className="absolute w-full h-10 bottom-0 bg-gradient-to-t from-black" >
                                 <h1 className="text-white absolute right-1 bottom-1 hover:underline hover:font-semibold"><i className="fa-solid fa-camera mr-2"></i><a href={item.photographer_url} className="cursor-pointer" target="_blank" rel="noreferrer">{item.photographer}</a></h1>
@@ -46,9 +53,16 @@ function Gallery() {
                         onClick={() => handlepage("plus")}>
                         Next<i className="fa-solid fa-caret-right ml-2"></i>
                     </div> : null}
-
             </div>
-
+            {isShow ? <div className="bg-[rgba(0,0,0,0.5)] bg-t w-full h-[100vh] fixed top-0 flex justify-center items-center">
+                <div className="max-w-[80%] max-h-[80%] relative">
+                    <img src={image} className="max-w-full max-h-[80vh]" />
+                    <div className="absolute -top-10 -right-12 text-3xl text-white border-4 px-2 rounded-full border-white hover:text-indigo-900 hover:border-indigo-900 cursor-pointer active:text-indigo-600 active:border-indigo-600"
+                        onClick={() => setIsShow(false)}>
+                        <i className="fa-solid fa-xmark"></i>
+                    </div>
+                </div>
+            </div> : null}
         </>
     )
 }
